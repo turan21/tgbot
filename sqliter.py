51ce070d18e3c461e6ffe98ee1737d480a058b3f
turan21 <turan.nktl@gmail.com>
2020-06-16 18:18:38 +0600

    update_count added sqlite3

--- sqliter.py
+++ sqliter.py
@@ -1,18 +1,89 @@
 import sqlite3
+from sqlite3 import Error
 
-db = sqlite3.connect('users.db')
-cur = db.cursor()
 
-cur.execute("""CREATE TABLE IF NOT EXISTS users (user_id INTEGER NOT NULL PRIMARY KEY, username INTEGER NOT NULL,file_path TEXT, age INTEGER, gender INTEGER)""")
-db.commit()
+# db = sqlite3.connect('words.db')
+# cur = db.cursor()
+#
+# cur.execute(
+#     """CREATE TABLE IF NOT EXISTS words (words_id INTEGER NOT NULL PRIMARY KEY, content VARCHAR[150] NOT NULL)""")
+# db.commit()
+#
+#
+# def write_db(content):
+#     cur.execute("SELECT words_id FROM words")
+#     content_text = str(content)
+#     cur.execute('insert into words(content) values (?)', (content_text,))
+#     db.commit()
+#
+#
+# write_db("Алардын чоңдору – чыгыш жактан куюлган Түп менен Жыргалаң суулары")
 
-username = int(input("username : "))
-file_path = input("Path : ")
-age = int(input("age : "))
-gender = int(input("gender : "))
-data_person_name = [(username, file_path,age,gender),]
+class Words():
+    WORDS_LIST = []
 
-cur.execute("SELECT user_id FROM users")
-cur.executemany('INSERT INTO users(username, file_path, age, gender) VALUES (?,?,?,?)', data_person_name)
-db.commit()
 
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def select_all_tasks(conn):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT content FROM words")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        Words.WORDS_LIST.append(row)
+        # print(row)
+
+
+
+
+# def select_task_by_priority(conn, priority):
+#     """
+#     Query tasks by priority
+#     :param conn: the Connection object
+#     :param priority:
+#     :return:
+#     """
+#     cur = conn.cursor()
+#     cur.execute("SELECT * FROM tasks WHERE priority=?", (priority,))
+#
+#     rows = cur.fetchall()
+#
+#     for row in rows:
+#         print(row)
+
+
+def main():
+    database = "words.db"
+
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+        # print("1. Query task by priority:")
+        # select_task_by_priority(conn, 1)
+
+        print("2. Query all tasks")
+        select_all_tasks(conn)
+
+
+if __name__ == '__main__':
+    main()
--- voice.py
+++ voice.py
@@ -1,5 +1,7 @@
+import sqlite3
 import time, telepot
 import os, shutil
+import sqliter
 from pathlib import Path
 from telepot.namedtuple import InlineKeyboardButton, InlineKeyboardMarkup
 from telepot.loop import MessageLoop
@@ -7,14 +9,55 @@ from telepot.delegate import per_chat_id, create_open, pave_event_space, per_cal
     include_callback_query_chat_id
 
 
+def write_db(username, file_path, age, gender, first_name):
+    db = sqlite3.connect('users.db')
+    cur = db.cursor()
+
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS users (user_id INTEGER NOT NULL PRIMARY KEY, username INTEGER NOT NULL UNIQUE,file_path TEXT, age INTEGER, gender INTEGER, first_name TEXT,count INTEGER)""")
+    db.commit()
+    data_person_name = [(username, file_path, age, gender, first_name), ]
+    cur.execute("SELECT user_id FROM users")
+    cur.executemany('INSERT OR IGNORE INTO users(username, file_path, age, gender, first_name) VALUES (?,?,?,?,?)',
+                    data_person_name)
+    db.commit()
+
+
+def get_count(username):
+    db = sqlite3.connect('users.db')
+    cur = db.cursor()
+    cur.execute(f"""SELECT count from users
+WHERE username = {username}""")
+    count = cur.fetchone()[0]
+    print(count)
+    return count
+
+
+def update_count(count, username):
+    db = sqlite3.connect('users.db')
+    cur = db.cursor()
+    cur.execute("SELECT username FROM users")
+    cur.execute(f"""UPDATE users
+SET count = {count}
+WHERE username = {username};""")
+    db.commit()
+
+
 class Foo(object):
-    CONST_NAME = ""
+    CONST_NAME = {"username": 0}
+    USER_INFO = []
+    gender = {"username": 0}
+    count = {"username": 0}
+
+
+age_list = [1, 2, 3, 4, 5, 6]
 
 
 class Voice(telepot.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
         super(Voice, self).__init__(*args, **kwargs)
         self._id = 0
+        sqliter.main()
 
     def on_chat_message(self, msg):
         content_type, chat_type, chat_id, msg_date, msg_id = telepot.glance(msg, flavor='chat', long=True)
@@ -25,36 +68,54 @@ class Voice(telepot.helper.ChatHandler):
             ]
             )
             bot.sendMessage(chat_id, 'Прослушайте заново и подтвердите', reply_markup=keyboard)
-            Foo.CONST_NAME = msg
+            Foo.CONST_NAME[msg['from']['id']] = msg
             return None
         elif content_type == 'text':
+            Foo.CONST_NAME[msg['from']['id']] = msg
             if msg['text'] == '/start':
-                bot.sendMessage(chat_id, 'Privet! ' + msg['from']['first_name'])
-                print(msg)
+                Foo.count[str(msg['from']['id'])] = 0
+                update_count(0, int(msg['from']['id']))
+                Path(f'/home/pc/PycharmProjects/tgbot/files/{msg["from"]["id"]}').mkdir(parents=True, exist_ok=True)
+                bot.sendMessage(chat_id,
+                                'Доброго времени суток! ' + msg['from']['first_name'] + '\n Зарегистрируйтесь /reg')
             elif msg['text'] == '/reg':
-                bot.sendMessage(chat_id, "Registration")
+                markup = InlineKeyboardMarkup(inline_keyboard=[
+                    [InlineKeyboardButton(text="Мужской", callback_data='male'),
+                     InlineKeyboardButton(text="Женский", callback_data='female'), ]
+                ]
+                )
+                bot.sendMessage(chat_id, "Выберите ваш пол", reply_markup=markup)
+
+
             elif msg['text'] == '/my':
                 bot.sendMessage(chat_id, msg['from']['first_name'])
-            elif msg['text'] == '/my':
-                bot.sendMessage(chat_id, msg['from']['first_name'])
+                if Foo.count[str(msg['from']['id'])] < len(sqliter.Words.WORDS_LIST) - 1:
+
+                    Foo.count[str(msg['from']['id'])] += 1
+                    update_count(int(Foo.count[str(msg['from']['id'])]), int(msg['from']['id']))
+                else:
+                    bot.sendMessage(chat_id, "Wait for texts!\n try again later")
+                    print(Foo.count)
             elif msg['text'] == '/help':
                 bot.sendMessage(chat_id, "registration - /reg\n my name - /my\n help - /help")
             else:
-                bot.sendMessage(chat_id, "registration - /reg\n my name - /my\n help - /help")
+                bot.sendMessage(chat_id,
+                                str(sqliter.Words.WORDS_LIST[get_count(msg["from"]["id"])]).strip("()'',"))
 
     def on_callback_query(self, msg):
         query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
-        content_type, chat_type, chat_id, msg_date, msg_id = telepot.glance(Foo.CONST_NAME, flavor='chat', long=True)
+        content_type, chat_type, chat_id, msg_date, msg_id = telepot.glance(Foo.CONST_NAME[msg['from']['id']],
+                                                                            flavor='chat', long=True)
         if query_data == 'yes':
             bot.editMessageText((msg['from']['id'], msg['message']['message_id']), "Отправлено")
-            duration = Foo.CONST_NAME['voice']['duration']
+            duration = Foo.CONST_NAME[msg['from']['id']]['voice']['duration']
             if duration > 30:
                 bot.sendMessage(chat_id, ">30 секунд")
                 return
 
-            file_name = Foo.CONST_NAME['voice']['file_id']
-            bot.download_file(Foo.CONST_NAME['voice']['file_id'], "{0}.ogg".format(file_name))
-            my_file = Path("./{0}.ogg".format(Foo.CONST_NAME['voice']['file_id']))
+            file_name = Foo.CONST_NAME[msg['from']['id']]['voice']['file_id']
+            bot.download_file(Foo.CONST_NAME[msg['from']['id']]['voice']['file_id'], "{0}.ogg".format(file_name))
+            my_file = Path("./{0}.ogg".format(Foo.CONST_NAME[msg['from']['id']]['voice']['file_id']))
 
             while not my_file.is_file():
                 time.sleep(2)
@@ -62,9 +123,51 @@ class Voice(telepot.helper.ChatHandler):
             start_time = time.time()
             elapsed_time = time.time() - start_time
             print("Time Elapsed: ", elapsed_time)
-            shutil.move(f'/home/pc/PycharmProjects/tgbot/{my_file}', f'/home/pc/PycharmProjects/tgbot/files/{my_file}')
+            shutil.move(f'/home/pc/PycharmProjects/tgbot/{my_file}',
+                        f'/home/pc/PycharmProjects/tgbot/files/{msg["from"]["id"]}/{my_file}')
         elif query_data == 'no':
             bot.editMessageText((msg['from']['id'], msg['message']['message_id']), "Запишите заново")
+        elif query_data == 'male':
+            Foo.gender[msg['from']['id']] = 0
+            markup = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text="6-12", callback_data=1),
+                 InlineKeyboardButton(text="12-18", callback_data=2)],
+                [InlineKeyboardButton(text="18-24", callback_data=3),
+                 InlineKeyboardButton(text="24-30", callback_data=4)],
+                [InlineKeyboardButton(text="30-40", callback_data=5),
+                 InlineKeyboardButton(text="40-50", callback_data=6)]
+            ]
+            )
+            bot.editMessageText((msg['from']['id'], msg['message']['message_id']), "Сколько вам лет?",
+                                reply_markup=markup)
+
+        elif query_data == 'female':
+            Foo.gender[msg['from']['id']] = 1
+            markup = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text="6-12", callback_data=1),
+                 InlineKeyboardButton(text="12-18", callback_data=2)],
+                [InlineKeyboardButton(text="18-24", callback_data=3),
+                 InlineKeyboardButton(text="24-30", callback_data=4)],
+                [InlineKeyboardButton(text="30-40", callback_data=5),
+                 InlineKeyboardButton(text="40-50", callback_data=6)]
+
+            ]
+            )
+            bot.editMessageText((msg['from']['id'], msg['message']['message_id']), "Сколько вам лет?",
+                                reply_markup=markup)
+        elif int(query_data) in age_list:
+            write_db(Foo.CONST_NAME[msg['from']['id']]['from']['id'],
+                     f'/home/pc/PycharmProjects/tgbot/files/{Foo.CONST_NAME[msg["from"]["id"]]["from"]["id"]}',
+                     query_data, Foo.gender[msg['from']['id']],
+                     Foo.CONST_NAME[msg['from']['id']]['from']['first_name'])
+            bot.editMessageText((msg['from']['id'], msg['message']['message_id']), "Регистрация прошла успешно")
+        #     markup = InlineKeyboardMarkup(inline_keyboard=[
+        #         [InlineKeyboardButton(text=" ▶️", callback_data='next')],
+        #     ]
+        #     )
+        #     bot.sendMessage(chat_id,"Нашмите на кнопку чтобы начать",reply_markup=markup)
+        # elif query_data == 'next':
+        #     bot.sendMessage(chat_id,"...")
 
 
 bot = telepot.DelegatorBot("1079116810:AAFKRqfx1XQhj6wG5jDifUUiHWsjtNpEpA4", [
